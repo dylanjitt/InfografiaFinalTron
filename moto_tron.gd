476c479c44961e433d4519933f27c19f08d4aa88
dylanJitton <dylanjitton2.0@gmail.com>
2023-09-15 00:28:16 -0400

    TronV2

--- moto_tron.gd
+++ moto_tron.gd
@@ -3,52 +3,60 @@ extends CharacterBody2D
 signal turn()
 signal updatePoints(position)
 signal playerDead()
+signal moving()
 @export var SPEED = 500
 var state = ""
+var moto_dead=false
 
 func _physics_process(delta):
 	#print(state)
 	#print(global_position)
-	if state != "":
-		emit_signal("updatePoints",global_position)
-	if Input.is_action_just_pressed("p1_left"):
-		if state != "right":
-			$AnimationPlayer.play("left")
-			velocity = Vector2(-1, 0) * SPEED 
-			print("left")
-			emit_signal("turn")
+	if moto_dead==false:
+		if state != "":
+			emit_signal("updatePoints",global_position)
+			emit_signal("moving")
+		if Input.is_action_just_pressed("p1_left"):
+			if state != "right":
+				$AnimationPlayer.play("left")
+				velocity = Vector2(-1, 0) * SPEED 
+				state="left"
+				print(state)
+				emit_signal("turn")
 		
-	if Input.is_action_just_pressed("p1_right"):
-		if state != "left":
-			$AnimationPlayer.play("right")
-			velocity = Vector2(1, 0) * SPEED
-			state = "right" 
-			print("right")
-			emit_signal("turn")
-			#emit_signal("turn",global_position)
+		if Input.is_action_just_pressed("p1_right"):
+			if state != "left":
+				$AnimationPlayer.play("right")
+				velocity = Vector2(1, 0) * SPEED
+				state = "right"
+				print(state) 
+				#print("right")
+				emit_signal("turn")
+				#emit_signal("turn",global_position)
 		
-	if Input.is_action_just_pressed("p1_up"):
-		if state != "down":
-			$AnimationPlayer.play("up")
-			velocity = Vector2(0, -1) * SPEED 
-			state = "up"
-			emit_signal("turn")
-			print("up")
-			#emit_signal("turn",global_position)
+		if Input.is_action_just_pressed("p1_up"):
+			if state != "down":
+				$AnimationPlayer.play("up")
+				velocity = Vector2(0, -1) * SPEED 
+				state = "up"
+				print(state)
+				emit_signal("turn")
+				#print("up")
+				#emit_signal("turn",global_position)
 			
-	if Input.is_action_just_pressed("p1_down"):
-		if state != "up":
-			$AnimationPlayer.play("down")
-			velocity = Vector2(0, 1) * SPEED 
-			state = "down"
-			emit_signal("turn")
-			print("down")
+		if Input.is_action_just_pressed("p1_down"):
+			if state != "up":
+				$AnimationPlayer.play("down")
+				velocity = Vector2(0, 1) * SPEED 
+				state = "down"
+				print(state)
+				emit_signal("turn")
+				#print("down")
 			
-	if Input.is_action_just_pressed("pause"):
-		velocity=Vector2(0,0)
-			#emit_signal("turn",global_position)
-	emit_signal("updatePoints",global_position)
-	move_and_slide()
+		if Input.is_action_just_pressed("pause"):
+			velocity=Vector2(0,0)
+				#emit_signal("turn",global_position)
+		#emit_signal("updatePoints",global_position)
+		move_and_slide()
 
 
 #func _on_hitbox_area_entered(area):
@@ -57,4 +65,5 @@ func _physics_process(delta):
 func dead():
 	print("colision detected")
 	velocity=Vector2(0,0)
+	moto_dead=true
 	emit_signal("playerDead")
--- trail1.gd
+++ trail1.gd
@@ -5,50 +5,64 @@ var segmentScene = preload("res://hitbox.tscn")
 var countInsance=0
 var currentPos=0
 var turns=0
+var dead = false
+var moving = false
 signal collision(area)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _physics_process(delta):
-	remove_first_segments(10)
+	remove_first_segments(turns)
 
 func _on_moto_update_points(position):
-	add_point(position)
-	countInsance+=1
-	#var moto_tron= $Moto
-	if countInsance%10==0:
-		currentPos=position
-		for i in (points.size() - 1):
-			var segment_instance = segmentScene.instantiate()
-			segment_instance.position=currentPos
-		#print(segment_instance.position)
-			segments.append(segment_instance)
-			add_child(segment_instance)
-	print(segments.size())
-	if points.size()==200:
-		remove_point(0)
-		#if segments.size()>=100:
-			#var removed_segment = segments.pop_front()
+	if dead == false:#&&moving==true:
+		add_point(position)
+		countInsance+=1
+		#var moto_tron= $Moto
+		if countInsance%5==0:
+			currentPos=position
+			print("-----------------",countInsance)
+			for i in (points.size() - 1):
+				var segment_instance = segmentScene.instantiate()
+				segment_instance.position=currentPos
+				#print(segment_instance.position)
+				segments.append(segment_instance)
+				add_child(segment_instance)
+		
+		if points.size()==150:
+			remove_point(0)
+			#if segments.size()>=100:
+				#var removed_segment = segments.pop_front()
 		if turns<=20:
-			remove_first_segments(10*turns)
+			remove_first_segments(1*turns)
+	print(segments.size())
 		
 
 func remove_first_segments(count):
 	for i in range(count):
 		if segments.size() > 0:
 			var removed_segment = segments[0]
-			segments.remove_at(0)
 			remove_child(removed_segment)
-
-
-
+			segments.remove_at(0)
 
 func _on_moto_turn():
 	#remove_first_segments(20)
-	if turns<=20:
-		turns+=2
+	#if turns<=10:
+	#	turns+=2
+	#	remove_first_segments(10)
+	#else:
+	#	remove_first_segments(10*turns)
+	turns+=1
+	print(turns)
+	remove_first_segments(20*turns)
 
 
 func _on_moto_player_dead():
-	pass # Replace with function body.
+	clear_points()
+	remove_first_segments(segments.size())
+	dead=true
+
+
+func _on_moto_moving():
+	moving==true
